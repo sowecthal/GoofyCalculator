ca2c4287d5186ebe22ae3a291e02ecc6ce870256
sowecthal <sowecthal@gmail.com>
2024-04-03 19:39:57 +0700

    Feat: Added new feautures to handleBalance and handleRegister

--- lib/commandHandler.py
+++ lib/commandHandler.py
@@ -1,6 +1,6 @@
 import re
+import hashlib
 
-from hashlib import md5
 from enum import Enum
 from dataclasses import dataclass
 from .database import Database
@@ -30,7 +30,9 @@ class CommandHandler:
         self.processed_users = processed_users
 
     async def handleCommand(self, command, client_connection):
-        action, args = await self.parseCommand(command)
+        action, args_str = await self.parseCommand(command)
+        args = args_str.split() if args_str else []
+        
         if not action:
             raise CommandException('Error: Invalid command syntax')
         func = getattr(self, f'handle{action.capitalize()}')
@@ -39,25 +41,35 @@ class CommandHandler:
         return await func(args, client_connection)
 
     async def parseCommand(self, command):
-        match = re.match(r'([a-zA-Z]+)\s*(\S+)?', command)
+        match = re.match(r'([a-zA-Z]+)\s*(.*)', command)
+
         if match:
             return match.groups()
         return None, None
 
-    async def handleRegister(self, _, client_connection):
+    async def handleRegister(self, args, client_connection):
         if client_connection.user.role != 'admin':
-            raise CommandException("Error: You can't register users")
-        
-        return f'You may proceed. Enter the word "login" and the login you wish to register'
+            raise CommandException("Error: You don't have permission to register users")
+        if len(args) != 2:
+            raise CommandException("Error: Invalid number of arguments. Usage: register <USERNAME> <PASSWORD>")
+
+        username, password = args
+        password_hash = hashlib.md5(password.encode()).hexdigest()
+
+        if await self.db.fetchUserByLogin(username):
+            raise CommandException("Error: Username already exists")
+
+        await self.db.insertNewUser(username, password_hash)
+
+        return f'User "{username}" registered successfully.'
 
     async def handleLogin(self, args, client_connection):
-        if not args:
-            raise CommandException('Error: Missing username')
-
         if client_connection.state != ConnectionState.AWAITING_LOGIN:
             raise CommandException('Error: Not currently awaiting for login')
-        
-        username = args.strip()
+        if len(args) != 1:
+            raise CommandException('Error: Invalid number of arguments. Usage: login <USERNAME>')
+
+        username = args[0]
         
         if not self.processed_users.get(username):
             user_data = await self.db.fetchUserByLogin(username)
@@ -75,51 +87,121 @@ class CommandHandler:
             return f'You may proceed. Enter the password for {username}. You have another session'
 
     async def handlePassword(self, args, client_connection):
-        if not args:
-            raise CommandException('Error: Missing password')
-        
         if client_connection.state != ConnectionState.AWAITING_PASSWORD:
             raise CommandException('Error: Not currently awaiting for password')
-        
-        password = args.strip()
+        if len(args) != 1:
+            raise CommandException('Error: Invalid number of arguments. Usage: password <PASSWORD>')
+
+        password = args[0]
         username = client_connection.user.login
 
+        password_hash = hashlib.md5(password.encode()).hexdigest()
         correct_password_hash = client_connection.user.password_hash
-        if correct_password_hash == password:
+
+        if correct_password_hash == password_hash:
             client_connection.state = ConnectionState.AUTHENTICATED
             return f'User "{username}" successfully authenticated'
         else:
             raise CommandException('Error: Incorrect password')
 
-
     async def handleCalc(self, args, client_connection):
         if client_connection.state != ConnectionState.AUTHENTICATED:
             raise CommandException('Error: No active login session')
-
+        if len(args) != 1:
+            raise CommandException('Error: Invalid number of arguments. Usage: calc <EXPRESSION>')
+        
         user = client_connection.user
+        expression = args[0]
 
         if client_connection.user.balance <= 0:
             raise CommandException('Error: Insufficient balance')
         try:
             client_connection.user.balance -= 1
-            result = eval(args)
+            result = eval(expression)
         except Exception as e:
             client_connection.user.balance += 1
             raise CommandException(f'Error: Invalid expression: {str(e)}')
     
-        await self.db.updateUserBalance(user.balance, user.id)
-        await self.db.insertCalculationHistory(user.id, args, result)
+        await self.db.updateUserBalance(user.id, user.balance)
+        await self.db.insertCalculationHistory(user.id, expression, result)
 
         return str(result)
     
-    async def handleBalance(self, _, client_connection):
+    async def handleBalance(self, args, client_connection):
         if client_connection.state != ConnectionState.AUTHENTICATED:
             raise CommandException('Error: No active login session')
+        if args and client_connection.user.role != 'admin':
+            raise CommandException('Error: You only have permission to check your own balance. Usage: balance')
+
+        if args:
+            if len(args) == 1:
+                username = args[0]
+                user_data = await self.db.fetchUserByLogin(username)
+                if not user_data:
+                    raise CommandException('Error: User not found')
+                return str(user_data['balance'])
+            elif len(args) == 3:
+                username, mode, value = args
+                user_data = await self.db.fetchUserByLogin(username)
+                if not user_data:
+                    raise CommandException('Error: User not found')
+                if mode != 'set' and mode != 'add':
+                    raise CommandException("Error: Invalid mode. Mode should be 'set' or 'add'")
+                
+                try:
+                    value = int(value)
+                except ValueError:
+                    raise CommandException('Error: Value must be an integer')
+                
+                user_balance = user_data['balance']
+                
+                if mode == 'set':
+                    user_balance = value
+                elif mode == 'add':
+                    user_balance += value
+
+                await self.db.updateUserBalance(user_data['id'], user_balance)
+                return f"Balance updated successfully for user '{username}'"
+
+            else:
+                raise CommandException('Error: Invalid number of arguments. Usage: balance <MODE> <VALUE>')
         return str(client_connection.user.balance)
+    
+        # if not args:
+        #     return str(client_connection.user.balance)
+        # elif len(args) == 1:
+        #     username = args[0]
+        #     user_data = await self.db.fetchUserByLogin(username)
+        #     if not user_data:
+        #         raise CommandException('Error: User not found')
+        #     return str(user.balance)
+        # elif len(args) == 3:
+        #     username, mode, value = args
+        #     user = self.processed_users[username]
+        #     if not user:
+        #         raise CommandException('Error: User not found')
+        #     if mode not in ['set', 'add']:
+        #         raise CommandException("Error: Invalid mode. Mode should be 'set' or 'add'")
+            
+        #     try:
+        #         value = int(value)
+        #     except ValueError:
+        #         raise CommandException('Error: Value must be an integer')
+
+        #     if mode == 'set':
+        #         user.balance = value
+        #     elif mode == 'add':
+        #         user.balance += value
+                
+        #     await self.db.updateUserBalance(user.balance, user.id)
+        #     return f"Balance updated successfully for user '{username}'"
+        # else:
+        #     raise CommandException('Error: Invalid number of arguments. Usage: balance <MODE> <VALUE>')
     
     async def handleLogout(self, _, client_connection):
         if client_connection.state != ConnectionState.AUTHENTICATED:
             raise CommandException('Error: No active login session to log out off')
+        
         username = client_connection.user.login
         client_connection.user = None
         client_connection.state = ConnectionState.AWAITING_LOGIN
